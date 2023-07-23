f3e68c327d2a7db0fb8d032d1a55af7d8d0bd315
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2023-07-23 20:21:45 +0530

    wow

--- src/components/Items/NewsItem.js
+++ src/components/Items/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {Title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
         <div className="card" style={{width:"18rem"}}>
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src={imageUrl?imageUrl : "https://www.digitaltrends.com/wp-content/uploads/2022/09/iPhone-14-Pro-App-Library.jpg?resize=1200%2C630&p=1"} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{Title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href={newsUrl} className="btn btn-primary" >Read more</a>
   </div>
 </div>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react'
 import NewsItem from './Items/NewsItem'
 
 export class News extends Component {
+   
+
+        constructor(){
+          super();
+          this.state = {
+            articles : [],
+            loading : false
+          }
+        }
+
+        async componentDidMount(){
+          let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=33f83513f9b94d259b8c48ee767f5437";
+          let data = await fetch(url);
+          let parsedData = await data.json();
+          this.setState({articles: parsedData.articles})
+        }
   render() {
     return (
       <div className='container my-3'>
         <h2>NewsEve - Top Headlines</h2>
          <div className='row'>
-            <div className='col-md-4'>
-        <NewsItem Title="Hell news" description="helldesc"/>
+        {this.state.articles.map((element)=>{
+         return <div className='col-md-4 my-4' key={element.url}>
+        <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
         </div>
-        <div className='col-md-4'>
-        <NewsItem Title="Hell news" description="helldesc"/>
-        </div>
-        <div className='col-md-4'>
-        <NewsItem Title="Hell news" description="helldesc"/>
-        </div>
+            })}
+       
         </div>
       </div>
     )
