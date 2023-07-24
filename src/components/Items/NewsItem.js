e032cbc25376d1165f96de83e19f550bf2ae4a1b
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2023-07-25 02:16:32 +0530

    wow

--- src/components/Items/NewsItem.js
+++ src/components/Items/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date} = this.props;
     return (
       <div>
         <div className="card" style={{width:"18rem"}}>
@@ -10,7 +10,8 @@ export default class NewsItem extends Component {
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
-    <a href={newsUrl} className="btn btn-primary" target='_blank' >Read more</a>
+    <p className='card-text'><small className='text-muted'>By {author?author:"Unknown"} on {date}</small></p>
+    <a href={newsUrl} rel="noreferrer" className="btn btn-primary" target='_blank' >Read more</a>
   </div>
 </div>
       </div>
